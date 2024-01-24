3b86a101c24f13f10fffd1bff7691640588e0032
Serhii Andrusenko <andrusenkoserhiy@gmail.com>
2024-01-24 15:50:45 +0200

    Cards, canvas prefabs

--- Assets/Scripts/Cards/CardData.cs
+++ Assets/Scripts/Cards/CardData.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 namespace Cards{
   [CreateAssetMenu(menuName = "GAME/Create CardData", fileName = "CardData", order = 0)]
   public class CardData : ScriptableObject{
+    public string NameText;
     public List<Tag> TagsList = new();
+    public Sprite FaceSprite;
   }
 }
