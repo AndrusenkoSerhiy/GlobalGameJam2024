a3f60ea7acee7ffe89099453003a85decef1b1ec
Serhii Andrusenko <andrusenkoserhiy@gmail.com>
2024-01-28 10:59:23 +0200

    Cards and Characters balances

--- Assets/Scripts/Cards/CardData.cs
+++ Assets/Scripts/Cards/CardData.cs
@@ -2,11 +2,13 @@ using System.Collections.Generic;
 using Tags;
 using UnityEngine;
 
-namespace Cards{
+namespace Cards {
   [CreateAssetMenu(menuName = "GAME/Create CardData", fileName = "CardData", order = 0)]
-  public class CardData : ScriptableObject{
-    public string NameText;
+  public class CardData : ScriptableObject {
+    public string NameText => names[Random.Range(0, names.Count)];
     public List<Tag> TagsList = new();
-    public Sprite FaceSprite;
+    [SerializeField] private List<Sprite> sprites = new();
+    [SerializeField] private List<string> names = new();
+    public Sprite FaceSprite => sprites[Random.Range(0, sprites.Count)];
   }
 }
